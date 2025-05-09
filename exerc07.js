7e3fe861fcf2ef6f6d67cba49f38c1cfd2b0a1ef
pauloromano3738 <pauloromano3738@gmail.com>
2025-05-09 09:38:32 -0300

    exercício 7 concluído

---
+++ exerc07.js
@@ -0,0 +1,22 @@
+// As maçãs custam R$ 0,30 se forem compradas menos do que uma dúzia, 
+// e R$ 0,25 se forem compradas pelo menos doze. Escreva um 
+// algoritmo que leia o número de maçãs compradas, 
+// calcule e escreva o valor total da compra.
+
+const prompt = require("prompt-sync")()
+
+let macasCompradas = Number(prompt("Quantas maças você vai comprar? "))
+
+let valorTotal = 0
+
+if (!isNaN(macasCompradas) && macasCompradas > 0) {
+    if (macasCompradas >= 12) {
+        valorTotal = macasCompradas * 0.25
+    } else {
+        valorTotal = macasCompradas * 0.30
+    }
+
+    console.log("o valor total da sua compra é: R$ " + valorTotal)
+} else {
+    console.log("dígite um valor válido!")
+}
